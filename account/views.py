806a724ba41de9b1e56f03dcd197e85cfa9e1f3a
6ejibiu <akimovpnz@live.com>
2020-04-17 19:03:52 +0300

    success registration. Add login after success registration

--- account/views.py
+++ account/views.py
@@ -1,7 +1,9 @@
+import logging
+
 from django.contrib import messages
+from django.contrib import auth
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
-from django.http import HttpResponse
 
 from .forms import AccountForm
 from .models import Account
@@ -13,12 +15,15 @@ def login(request):
         form_account = AccountForm(request.POST, request.FILES)
         if form.is_valid():
             user = form.save()
+            logging.info(f"create user with email={user.email}")
             if form_account.is_valid():
                 account = form_account.save(commit=False)
                 account.save()
                 account.user = user
+                logging.info(f'create avatar to user {user.email}')
             else:
                 account = Account.objects.create(user=user)
-            messages.add_message(request, messages.INFO, 'You success sign up')
+            auth.login(request, user)
+            messages.add_message(request, messages.SUCCESS, 'You success sign up')
             return render(request, 'index.html')
-    return HttpResponse("")
+    return render(request, 'index.html')
