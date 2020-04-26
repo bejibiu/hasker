d95d38c75f9bdff8525be3d4fe05ad669abae3aa
6ejibiu <akimovpnz@live.com>
2020-04-27 01:34:22 +0300

    change settings allow config

--- hasker/settings.py
+++ hasker/settings.py
@@ -25,10 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-if DEBUG:
-    ALLOWED_HOSTS = []
-else:
-    ALLOWED_HOSTS = ['hasker-simon.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['hasker-simon.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
