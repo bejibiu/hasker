c15806d1b0e029c98e8dfe5c86ec05fdf634577b
6ejibiu <akimovpnz@live.com>
2020-04-27 14:12:22 +0300

    fix debug settings. If not set debug -> django run as prod

--- hasker/settings.py
+++ hasker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
 ALLOWED_HOSTS = ['hasker-simon.herokuapp.com', '127.0.0.1']
 
