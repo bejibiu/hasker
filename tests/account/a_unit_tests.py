08a76e42e494b9a72c804d6956c9cdc8de65dfc9
6ejibiu <akimovpnz@live.com>
2020-05-02 16:10:07 +0300

    unit test to case #22

--- tests/account/a_unit_tests.py
+++ tests/account/a_unit_tests.py
@@ -19,63 +19,61 @@ def test_correct_resolve_account_settings():
     assert func.view_name == 'account_settings'
 
 
-@pytest.mark.django_db
-def test_create_user_account_with_avatar(client, avatar):
-    data = {
-        "email": "test@mail.com",
-        "username": "test",
-        "password1": "superPassword123",
-        "password2": "superPassword123",
-        "avatar": avatar
-    }
+class CreateAccountCase:
+    @pytest.mark.django_db
+    def test_create_user_account_with_avatar(self, client, avatar):
+        data = {
+            "email": "test@mail.com",
+            "username": "test",
+            "password1": "superPassword123",
+            "password2": "superPassword123",
+            "avatar": avatar
+        }
 
-    client.post(reverse('registration'), data=data)
-    user = User.objects.select_related('account').first()
+        client.post(reverse('registration'), data=data)
+        user = User.objects.select_related('account').first()
 
-    assert User.objects.count() == 1
-    assert user.account.avatar.name
+        assert User.objects.count() == 1
+        assert user.account.avatar.name
 
+    @pytest.mark.django_db
+    def test_create_user_account_with_post(self, client):
+        data = {
+            "email": "test@mail.com",
+            "username": "test",
+            "password1": "superPassword123",
+            "password2": "superPassword123",
+        }
 
-@pytest.mark.django_db
-def test_create_user_account_with_post(client):
-    data = {
-        "email": "test@mail.com",
-        "username": "test",
-        "password1": "superPassword123",
-        "password2": "superPassword123",
-    }
+        client.post(reverse('registration'), data=data)
+        user = User.objects.select_related('account').first()
+        assert User.objects.count() == 1
+        assert not user.account.avatar.name
 
-    client.post(reverse('registration'), data=data)
-    user = User.objects.select_related('account').first()
-    assert User.objects.count() == 1
-    assert not user.account.avatar.name
+    @pytest.mark.django_db
+    def test_login_with_post(self, client, user):
+        data = {
+            "username": f"{user.username}",
+            "password": "very_Strong_password!@# Z",
+        }
 
+        res = client.post(reverse('login'), data=data)
 
-@pytest.mark.django_db
-def test_login_with_post(client, user):
-    data = {
-        "username": f"{user.username}",
-        "password": "very_Strong_password!@# Z",
-    }
-
-    res = client.post(reverse('login'), data=data)
-
-    assert res.status_code == 302
-
-
-def test_not_available_settings_page_not_auth(client):
-    res = client.get(reverse('account_settings'))
-    assert res.status_code == 302
+        assert res.status_code == 302
 
 
-def test_available_settings_in_auth_client(authenticated_client):
-    res = authenticated_client.get(reverse('account_settings'))
-    assert res.status_code == 200
+class TestSettingsCase:
+    def test_not_available_settings_page_not_auth(self, client):
+        res = client.get(reverse('account_settings'))
+        assert res.status_code == 302
 
+    def test_available_settings_in_auth_client(self, authenticated_client):
+        res = authenticated_client.get(reverse('account_settings'))
+        assert res.status_code == 200
 
-def test_correct_template_for_settings(authenticated_client):
-    res = authenticated_client.get(reverse('account_settings'))
-    assert 'account/settings.html' in (t.name for t in res.templates)
+    def test_correct_template_for_settings(self, authenticated_client):
+        res = authenticated_client.get(reverse('account_settings'))
+        assert 'account/settings.html' in (t.name for t in res.templates)
 
 
 def test_save_new_email(authenticated_client, user):
--- tests/question_and_answer/qa_unit_tests.py
+++ tests/question_and_answer/qa_unit_tests.py
@@ -23,117 +23,106 @@ def test_home_page_content(client, db):
     assert "popular_questions" in res.context_data.keys()
 
 
-def test_popular_max_20_question(client, db, question_30):
-    res = client.get("/")
-    popular_question = res.context_data["popular_questions"]
-    assert len(popular_question) == 20
+class TestQuestionCase:
+    def test_popular_max_20_question(self, client, db, question_30):
+        res = client.get("/")
+        popular_question = res.context_data["popular_questions"]
+        assert len(popular_question) == 20
+
+    def test_fail_ask_not_auth_client(self, client):
+        data = {"title": "Title for message", "question": "is that a question?"}
+        res = client.post(reverse("save_question"), data)
+        assert res.status_code == 200
+
+    def test_save_question_without_tags(self, authenticated_client):
+        data = {"title": "Title for message", "text": "is that a question?", "tags": ""}
+        res = authenticated_client.post(reverse("save_question"), data)
+        assert Question.objects.all().count() == 1
+        assert Question.objects.first().title == data["title"]
+        assert res.status_code == 302
+
+    def test_save_question_with_tags(self, authenticated_client):
+        data = {"title": "Title for message", "text": "is that a question?", "tags": "1,2"}
+        res = authenticated_client.post(reverse("save_question"), data)
+        question = Question.objects.first()
+        assert res.status_code == 302
+        assert Question.objects.all().count() == 1
+        assert Question.objects.first().title == data["title"]
+        assert Tags.objects.count() == 2
+        assert question.votes == 0
+        assert question.answer_count == 0
+
+    def test_save_question_with_max_3tags(self, authenticated_client):
+        data = {
+            "title": "Title for message",
+            "text": "is that a question?",
+            "tags": "1,2,3,4",
+        }
+        res = authenticated_client.post(reverse("save_question"), data)
+        assert Tags.objects.count() == 3
+
+    def test_upper_votes(self, authenticated_client, question):
+        res = authenticated_client.get(f"{question.get_absolute_url()}up/")
+        assert question.votes == 1
+        res = authenticated_client.get(f"{question.get_absolute_url()}up/")
+        assert question.votes == 0
+
+    def test_downer_votes(self, authenticated_client, question):
+        res = authenticated_client.get(f"{question.get_absolute_url()}down/")
+        assert question.votes == -1
+        res = authenticated_client.get(f"{question.get_absolute_url()}down/")
+        assert question.votes == 0
+
+    def test_toggle_votes(self, authenticated_client, question):
+        authenticated_client.get(f"{question.get_absolute_url()}up/")
+        assert question.votes == 1
+        authenticated_client.get(f"{question.get_absolute_url()}down/")
+        assert question.votes == -1
 
 
-def test_fail_ask_not_auth_client(client):
-    data = {"title": "Title for message", "question": "is that a question?"}
-    res = client.post(reverse("save_question"), data)
-    assert res.status_code == 200
+class TestAnswer:
+    def test_upper_votes_to_answer(self, authenticated_client, answers):
+        answer = answers[0]
+        res = authenticated_client.get(f"{answer.get_absolute_url()}up/")
+        assert answer.votes == 1
+        res = authenticated_client.get(f"{answer.get_absolute_url()}up/")
+        assert answer.votes == 0
+
+    def test_set_answer_as_correct(self, authenticated_client, answers):
+        answer = answers[0]
+        authenticated_client.get(answer.get_absolute_url_to_toggle_answer_as_correct())
+        assert Answer.objects.get(pk=answer.pk).correct
+
+    def test_set_answer_as_correct_if_not_author(self, authenticated_client, answers):
+        answer = answers[0]
+        user = User.objects.create(username="new_user")
+        answer.question.author = user
+        answer.question.save()
+        res = authenticated_client.get(
+            answer.get_absolute_url_to_toggle_answer_as_correct()
+        )
+        assert not answer.correct
+        assert res.status_code == 403
+
+    def test_toggle_answer_correct(self, authenticated_client, answers):
+        answer = answers[0]
+        authenticated_client.get(answer.get_absolute_url_to_toggle_answer_as_correct())
+        assert Answer.objects.get(pk=answer.id).correct
+        authenticated_client.get(answer.get_absolute_url_to_toggle_answer_as_correct())
+        assert Answer.objects.get(pk=answer.id).correct is False
+
+    def test_toggle_answer_correct_after_set_another_answer_correct(self, authenticated_client, answers):
+        answer1 = answers[1]
+        answer2 = answers[2]
+        authenticated_client.get(answer1.get_absolute_url_to_toggle_answer_as_correct())
+        assert Answer.objects.get(pk=answer1.id).correct
+        authenticated_client.get(answer2.get_absolute_url_to_toggle_answer_as_correct())
+        assert Answer.objects.get(pk=answer1.id).correct is False
 
 
-def test_save_question_without_tags(authenticated_client):
-    data = {"title": "Title for message", "text": "is that a question?", "tags": ""}
-    res = authenticated_client.post(reverse("save_question"), data)
-    assert Question.objects.all().count() == 1
-    assert Question.objects.first().title == data["title"]
-    assert res.status_code == 302
-
-
-def test_save_question_with_tags(authenticated_client):
-    data = {"title": "Title for message", "text": "is that a question?", "tags": "1,2"}
-    res = authenticated_client.post(reverse("save_question"), data)
-    question = Question.objects.first()
-    assert res.status_code == 302
-    assert Question.objects.all().count() == 1
-    assert Question.objects.first().title == data["title"]
-    assert Tags.objects.count() == 2
-    assert question.votes == 0
-    assert question.answer_count == 0
-
-
-def test_save_question_with_max_3tags(authenticated_client):
-    data = {
-        "title": "Title for message",
-        "text": "is that a question?",
-        "tags": "1,2,3,4",
-    }
-    res = authenticated_client.post(reverse("save_question"), data)
-    assert Tags.objects.count() == 3
-
-
-def test_upper_votes(authenticated_client, question):
-    res = authenticated_client.get(f"{question.get_absolute_url()}up/")
-    assert question.votes == 1
-    res = authenticated_client.get(f"{question.get_absolute_url()}up/")
-    assert question.votes == 0
-
-
-def test_downer_votes(authenticated_client, question):
-    res = authenticated_client.get(f"{question.get_absolute_url()}down/")
-    assert question.votes == -1
-    res = authenticated_client.get(f"{question.get_absolute_url()}down/")
-    assert question.votes == 0
-
-
-def test_toggle_votes(authenticated_client, question):
-    authenticated_client.get(f"{question.get_absolute_url()}up/")
-    assert question.votes == 1
-    authenticated_client.get(f"{question.get_absolute_url()}down/")
-    assert question.votes == -1
-
-
-# answer toggle votes
-def test_upper_votes_to_answer(authenticated_client, answers):
-    answer = answers[0]
-    res = authenticated_client.get(f"{answer.get_absolute_url()}up/")
-    assert answer.votes == 1
-    res = authenticated_client.get(f"{answer.get_absolute_url()}up/")
-    assert answer.votes == 0
-
-
-def test_set_answer_as_correct(authenticated_client, answers):
-    answer = answers[0]
-    authenticated_client.get(answer.get_absolute_url_to_toggle_answer_as_correct())
-    assert Answer.objects.get(pk=answer.pk).correct
-
-
-def test_set_answer_as_correct_if_not_author(authenticated_client, answers):
-    answer = answers[0]
-    user = User.objects.create(username="new_user")
-    answer.question.author = user
-    answer.question.save()
-    res = authenticated_client.get(
-        answer.get_absolute_url_to_toggle_answer_as_correct()
-    )
-    assert not answer.correct
-    assert res.status_code == 403
-
-
-def test_toggle_answer_correct(authenticated_client, answers):
-    answer = answers[0]
-    authenticated_client.get(answer.get_absolute_url_to_toggle_answer_as_correct())
-    assert Answer.objects.get(pk=answer.id).correct
-    authenticated_client.get(answer.get_absolute_url_to_toggle_answer_as_correct())
-    assert Answer.objects.get(pk=answer.id).correct is False
-
-
-def test_toggle_answer_correct_after_set_another_answer_correct(
-    authenticated_client, answers
-):
-    answer1 = answers[1]
-    answer2 = answers[2]
-    authenticated_client.get(answer1.get_absolute_url_to_toggle_answer_as_correct())
-    assert Answer.objects.get(pk=answer1.id).correct
-    authenticated_client.get(answer2.get_absolute_url_to_toggle_answer_as_correct())
-    assert Answer.objects.get(pk=answer1.id).correct is False
-
-
-def test_tags_search(client, question):
-    res = client.get(
-        reverse("search_question"), {"q": f"tag:{question.tags.first().label}"}
-    )
-    assert res.context_data["object_list"].first() == question
+class TestSearch:
+    def test_tags_search(self, client, question):
+        res = client.get(
+            reverse("search_question"), {"q": f"tag:{question.tags.first().label}"}
+        )
+        assert res.context_data["object_list"].first() == question
